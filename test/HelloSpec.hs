a589ca9fb616eadcae3bd6ee6e3afb717255dd4d
Keisuke Kumada <kekumada@yahoo-corp.jp>
2017-02-09 11:46:53 +0900

    initial

---
+++ test/HelloSpec.hs
@@ -0,0 +1,11 @@
+module HelloSpec where
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "hello test" $ do
+        it "test" $ 3 `shouldBe` (3 :: Int)
+
+    describe "hello test2" $ do
+        it "hogehoge" $ "foo" `shouldBe` "foo"
